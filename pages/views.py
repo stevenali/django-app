d70793826c35093c12637702771c72f318ceee9a
Alican Kılıç <alicankilic01@hotmail.com>
2021-06-25 15:04:18 +0300

    make it better

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
 from django.http.response import JsonResponse
 from django.shortcuts import render
-from .models import Contact
 from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import EmailMessage , send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
 def index(request):
     return render(request,'index.html')
 def contact(request):
@@ -16,6 +17,12 @@ def contactbackend(request):
         message = request.POST["message"]
         email_subject1 = "merhaba "+name
         email_body1 = f"{email}     -     {message}"
+        html_content =  f"<h1>{message}</h1><h4>{email}     -     {message}</h4>"
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives("subject", text_content, 'noreply.aliwkochannels@gmail.com', ["alicankilic1905@hotmail.com"])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        
         email2 = EmailMessage(
             email_subject1 ,
             email_body1,
