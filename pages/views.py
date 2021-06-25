821d38d64fc9fd81e5833fdbfff5c615aa4a26ec
Alican Kılıç <alicankilic01@hotmail.com>
2021-06-25 14:32:37 +0300

    make it better

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,9 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from .models import Contact
 from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 def index(request):
     return render(request,'index.html')
 def contact(request):
@@ -13,7 +16,11 @@ def contactbackend(request):
         name = request.POST["name"]
         email = request.POST["email"]
         message = request.POST["message"]
-        Contact.objects.create(name=name, email=email, message = message)
+        html_content = f"<h1>{name}</h1><h4>{email}</h4><h4>{message}</h4>"
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives("subject", text_content, 'noreply.aliwkochannels@gmail.com', ["alicankilic1905@hotmail.com"])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
         return JsonResponse({"sa":"as"})
     else:
         return redirect("/")
--- proje/settings.py
+++ proje/settings.py
@@ -64,6 +64,12 @@ TEMPLATES = [
         },
     },
 ]
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'noreply.aliwkochannels@gmail.com'
+DEFAULT_FROM_EMAIL = 'noreply.aliwkochannels@gmail.com'
+EMAIL_HOST_PASSWORD = 'Fecliali1'
+EMAIL_PORT = 587
 
 WSGI_APPLICATION = 'proje.wsgi.application'
 
